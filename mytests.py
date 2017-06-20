18e290be381f9dd4c2c77f698c8b4942db1145e6
Tim Nelson <tdnelson@gmail.com>
2017-06-20 16:03:29 -0700

    Initial commit

---
+++ mytests.py
@@ -0,0 +1,3 @@
+import tournament
+
+tournament.registerPlayer('Will Nelson')
---
+++ tournament.sql
@@ -0,0 +1,21 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+DROP DATABASE IF EXISTS tournament;
+
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE players ( name TEXT,
+                       id SERIAL );
+
+CREATE TABLE matches ( player1 INTEGER,
+					   player2 INTEGER,
+					   winner INTEGER );
+
